80ad29da0309f1a7434e467e94478461b9c78869
BoofinBool <sioldead@mcmaster.ca>
2024-11-01 21:48:38 -0400

    Fixed a whoopsie - has login, registration, and shop pages

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,8 @@
 import os
+import config
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # Define the instance path inside the 'market' folder
 instance_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance')
@@ -11,8 +13,9 @@ os.makedirs(instance_path, exist_ok=True)
 app = Flask(__name__, instance_path=instance_path)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(instance_path, "shop.db")}'
-app.config['SECRET_KEY'] = 'b472c46cbe0c0ffb5d3ad861'
+app.config['SECRET_KEY'] = config.secret_key
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app) 
 
 from market import routes
 from market.models import Item
