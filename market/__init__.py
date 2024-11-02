7bf09aaedfb01ab67fcb5c63e7775bfbb7620d68
BoofinBool <sioldead@mcmaster.ca>
2024-11-02 02:01:57 -0400

    Added full login functionality. Added extra course page and terms and condition page shown underneath registration

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ import config
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # Define the instance path inside the 'market' folder
 instance_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance')
@@ -15,8 +16,10 @@ app = Flask(__name__, instance_path=instance_path)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(instance_path, "shop.db")}'
 app.config['SECRET_KEY'] = config.secret_key
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app) 
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'loginPage'
+login_manager.login_message_category = 'info'
 from market import routes
 from market.models import Item
 
--- market/routes.py
+++ market/routes.py
@@ -3,17 +3,26 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
 def homePage():
     return render_template('home.html')
 
+
 @app.route('/shop')
+@login_required
 def shopPage():
     items = Item.query.all()
     print(items)
     return render_template('shop.html', items=items)
+
+
+@app.route('/villanyCourse')
+@login_required
+def villanyCourse():
+    return render_template('villanyCourse.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def registerPage():
@@ -24,6 +33,9 @@ def registerPage():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'Welcome aboard {user_to_create.username}! Need to put together a scheme? <a href="{url_for("villanyCourse")}" class="alert-link"> Click here</a> to sign up for our Villany course!', category='success')
+
         return redirect(url_for('shopPage'))
     
     if form.errors != {}: # If there are not errors from the validations
@@ -32,7 +44,25 @@ def registerPage():
 
     return render_template('register.html', form=form)
 
+@app.route('/termsConditions')
+def termsPage():
+    return render_template('termsCond.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def loginPage():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Welcom back {attempted_user.username}! Browse some gizmos for some neerdowelling', category='success')
+            return redirect(url_for('shopPage'))
+        else:
+            flash('Invalid credentials! Please try again', category='danger')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logoutPage():
+    logout_user()
+    flash('You have been logged out!', category='info')
+    return redirect(url_for('homePage'))
